01d44ddb7983f599c33b388337d26ab6ea18b9e9
Vamsi Talupula <tanavamsikrishna@outlook.com>
2023-10-15 13:33:32 +0530

    Added ifc

--- src/itertools.rs
+++ src/itertools.rs
@@ -4,3 +4,10 @@ macro_rules! imc {
         $variable_name.iter().map($lambda).collect()
     };
 }
+
+#[macro_export]
+macro_rules! ifc {
+    ($variable_name:ident, $lambda:expr) => {
+        $variable_name.iter().filter($lambda).collect()
+    };
+}
